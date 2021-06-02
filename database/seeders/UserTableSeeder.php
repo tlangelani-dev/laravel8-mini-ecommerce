533d50898a4c99df616cc0c79bb1a353eb23ee33
Tlangelani Mboweni <tlangelani@tinmandigital.co.za>
2021-06-02 16:01:32 +0200

    add user table seeder

---
+++ database/seeders/UserTableSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class UserTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->truncate();
+        User::factory(20)->create();
+    }
+}
